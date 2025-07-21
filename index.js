efc541adb90ea9f6d2c20991b4b7b1aee49da6a5
Ishan <ishanjungmalla@gmail.com>
2025-07-21 18:44:12 +0545

    toggle menu feature added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const downloadCv = document.getElementById("download-cv");
 const copyMail = document.getElementById("copy-email");
+const burgerMenu = document.getElementsByClassName("burger-menu")[0];
+const mobileNav = document.getElementsByClassName("mobile-nav")[0];
 
 downloadCv.addEventListener("click", () => {
   const link = document.createElement("a");
@@ -18,4 +20,8 @@ copyMail.addEventListener("click", () => {
     .catch((err) => {
       console.error("Failed to copy email: ", err);
     });
+});
+
+burgerMenu.addEventListener("click", () => {
+  mobileNav.classList.toggle("toggle");
 });
